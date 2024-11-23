e95a4167cdbe6b65cc094474ae39277d32622f4d
Kason Steiner <kasonsteiner27@gmail.com>
2024-11-23 23:10:36 +0000

    Changed program to keep track of user win total, and have user controls when to end program.

--- .ccls-cache/@home@runner@Chapter-6-Exercise-24-Rock-Paper-Scissors-1/Chp6-24_Kason_Steiner.cpp
+++ .ccls-cache/@home@runner@Chapter-6-Exercise-24-Rock-Paper-Scissors-1/Chp6-24_Kason_Steiner.cpp
@@ -1,16 +1,13 @@
 /*Title: Chapter 6 Exercise 24 - Rock Paper Scissors 
-File:Chp6-24_Kason_Steiner
+File:Chp6Exc24_Kason_Steiner
 Programmer: Kason Steiner
 Date: 11/2024
-When the program begins, a random number in the range of 1 through 3 is generated. If the number is 1, then the computer has chosen rock. 
-If the number is 2, then the computer has chosen paper. If the number is 3, then the computer has chosen scissors. (Don’t display the computer’s choice yet.)
-2. The user enters his or her choice of “rock”, “paper”, or “scissors” at the keyboard. (You can use a menu if you prefer.) 
-3. The computer’s choice is displayed.
-4. A winner is selected according to the following rules:
-If one player chooses rock and the other player chooses scissors, then rock wins. (The rock smashes the scissors.)
-If one player chooses scissors and the other player chooses paper, then scissors wins. (Scissors cuts paper.)
-If one player chooses paper and the other player chooses rock, then paper wins. (Paper wraps rock.)
-If both players make the same choice, the game must be played again to determine the winner.
+Revise your original Rock Paper Scissors Game so that it is repeatable
+using  sentinel value to end. On each iteration of the game you should
+accept the name of the player and keep count of how many times they
+have won. Modify your program using a new branch of your project so that
+you preserve the orginal main branch from Part 1.
+Submit the code file as well as a screenshot of your testing.
 */
 
 #include <iostream>
@@ -19,19 +16,36 @@ using namespace std;
 
 int cpuChoice();
 int playerChoice();
-void winner(int, int);
+void winner(int, int, int&);
 
 int main() { 
-
-  int cpu = cpuChoice(); 
-  int player;
-
+  char playAgain = 0;
+  string name;   //players name
+  int winCount = 0; //win counter
   cout << "Rock Paper Scissors Game\n";
   cout << "------------------------\n";
+  cout << "Enter your name: ";
+  cin >> name;
 
-  player = playerChoice();
-  cout << "Computer's choice: " << cpu << endl;
-  winner(player, cpu);
+  while (playAgain != 'n' && playAgain != 'N')
+  {  
+    int cpu = cpuChoice(); 
+    int player;    //players choice
+
+    player = playerChoice();
+    cout << "Computer's choice: " << cpu << endl;
+    winner(player, cpu, winCount); 
+
+
+    cout << "Number of games " << name << " has won: " << winCount << endl;
+    cout << "Would you like to play again? ";
+    cout << "Press (y) to play again. Press (n) to quit.\n";
+    cin >> playAgain;
+
+
+  }
+    cout << "The game is over. ";
+
 
 return 0; 
 }
@@ -46,30 +60,47 @@ int cpuChoice()
 int playerChoice()
 {
   int choice;
-
     cout << "1. Rock\n";
     cout << "2. Paper\n";
     cout << "3. Scissors\n";
     cout << "Enter your choice (1, 2, or 3): ";
     cin >> choice;
+
+  while (choice < 1 || choice > 3)
+    {
+      cout << "Please enter a number 1-3\n";
+      cout << "1. Rock\n";
+      cout << "2. Paper\n";
+      cout << "3. Scissors\n";
+      cout << "Enter your choice (1, 2, or 3): ";
+      cin >> choice;
+    }
   return choice;
 }
 
-void winner(int player, int cpu)
+void winner(int player, int cpu, int& winCount) 
 {
-
   if (player == 1 && cpu == 2)
-    cout << "Paper wraps the rock. You lose!";
+    cout << "Paper wraps the rock. You lose!\n\n";
   if (player == 2 && cpu ==1)
-    cout << "Paper wraps the rock. You win!";
+  {
+    cout << "Paper wraps the rock. You win!\n\n";
+    winCount++; //adds player win
+  }
   if (player == 1 && cpu == 3)
-    cout << "Rock smashes scissors. You win!";
+  {
+    cout << "Rock smashes scissors. You win!\n\n";
+    winCount++; //adds player win
+  }
   if (player == 3 && cpu == 1)
-    cout << "Rock smashes scissors. You lose!";
+    cout << "Rock smashes scissors. You lose!\n\n";
   if (player == 2 && cpu == 3)
-    cout << "Scissors cuts paper. You lose!";
+    cout << "Scissors cuts paper. You lose!\n\n";
   if (player == 3 && cpu == 2)
-    cout << "Scissors cuts paper. You win!";
+  {
+    cout << "Scissors cuts paper. You win!\n\n";
+    winCount++; //adds player win
+  }
   if (player == cpu)
-    cout << "It's  draw! Try again!";
+    cout << "It's  draw!\n\n";
 }
